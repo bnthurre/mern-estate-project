ca46d53701d8dfec0c0842b59ebbfb9a8c6eab68
bnthurre <bushrahurre959@gmail.com>
2024-03-12 11:40:07 +0300

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
+
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
+
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/signin' element={<SignIn/>}></Route>
-      <Route path='/signup' element={<SignIn />}></Route>
+      <Route path='/signup' element={<SignUp />}></Route>
       <Route path='/about' element={<About/>}></Route>
       <Route path='/profile' element={<Profile/>}></Route>
     </Routes>
